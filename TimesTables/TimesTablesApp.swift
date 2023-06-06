3a5ee26ec04db2d559844ef3fad00ce0cd8bec16
David Granger <dpgranger8@gmail.com>
2023-06-05 22:38:28 -0600

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by David Granger on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
